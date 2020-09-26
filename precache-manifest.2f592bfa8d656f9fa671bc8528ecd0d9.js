9d0cb581108ada08fa7b01d5050cbc7b37e83ed8
first commit <sanju5004@gmail.com>
2020-09-25 17:56:33 -0700

    Updates

---
+++ precache-manifest.2f592bfa8d656f9fa671bc8528ecd0d9.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5448a9363622b776f2b4fd67b6ba15",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "6b735feccdbf3e817936",
+    "url": "/reacttest/static/css/2.e52e4b08.chunk.css"
+  },
+  {
+    "revision": "99bd0140bddb9f9acf77",
+    "url": "/reacttest/static/css/main.ba80e23b.chunk.css"
+  },
+  {
+    "revision": "6b735feccdbf3e817936",
+    "url": "/reacttest/static/js/2.5857cb95.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/reacttest/static/js/2.5857cb95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99bd0140bddb9f9acf77",
+    "url": "/reacttest/static/js/main.76ece968.chunk.js"
+  },
+  {
+    "revision": "7b65aa31a6ca0391dad8",
+    "url": "/reacttest/static/js/runtime-main.82a5519d.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/reacttest/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/reacttest/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/reacttest/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/reacttest/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/reacttest/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/reacttest/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/reacttest/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/reacttest/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/reacttest/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/reacttest/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/reacttest/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/reacttest/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/reacttest/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/reacttest/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/reacttest/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/reacttest/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/reacttest/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "407e1e22494681c8b5062cecfe9bd79b",
+    "url": "/reacttest/static/media/gifsys.407e1e22.gif"
+  }
+]);
